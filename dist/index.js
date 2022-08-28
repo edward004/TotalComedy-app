f4dd76868253917cb5be7839df1f1180cec13734
Edward Henson <edwardhenson@icloud.com>
2022-08-28 14:21:17 +0100

    fix

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
-app.use('/', function (req, res) { return res.sendFile("Fork\TotalComedy-app\src\components\app-route\app-route.tsx"); });
+app.use('/', function (req, res) { return res.send("Hello"); });
 app.listen(3000, function () { return console.log("Listening"); });
 //# sourceMappingURL=index.js.map
